7d4dfba7d7973bf7403424bc6494e48602271535
Rex Root <rex@mail.206mail.com>
2020-10-29 14:21:04 -0700

    Ran dupes.c through indent

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all: dupes.c
 	${RM} ${HOME}/local/bin/dupes
+	DEPS = ${HOME}/dev/workspaces/dupes/dupes.h
 	gcc -O3 dupes.c -o ${HOME}/local/bin/dupes -I.
-	
+
 
 clean:
 	$(RM) dupes
--- dupes.h
+++ dupes.h
@@ -1,12 +1,24 @@
-#ifndef DUPES_H_   
+#ifndef DUPES_H_
 #define DUPES_H_
 
 // Define Maximum Number of Files.  Currently 5 Billion
 #define maximum_number_of_files 5000000000
 
+char           *directory_name[maximum_number_of_files];
+char           *file_name[maximum_number_of_files];
+long		file_size [maximum_number_of_files];
+int		filenum = 0;
+char           *file_name[];
+int		hit = 0;
+int		delete = 0;
+int		compnum = 0;
+int		tolerance = 0;
+unsigned long int pass = 0;
+
+
+
 char *get_current_directory (void);
-int get_file (const char *base_name, const struct stat *info, const int typeflag,
-	      struct FTW *pathinfo);
+int get_curr_file (const char *base_name, const struct stat *info, const int typeflag, struct FTW *pathinfo);
 int get_file_list (char *pwd);
 int check_existing (char *dirc, char *base_name, uintptr_t tfilesize);
 int add_file_to_memory (char *dirc, char *base_name, uintptr_t tfilesize);
@@ -16,5 +28,6 @@ char *file_name[maximum_number_of_files];
 long file_size[maximum_number_of_files];
 int delete_files(char *full_name);
 void help(void);
+
 
 #endif // DUPES_H_
